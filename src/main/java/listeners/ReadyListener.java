2045466861658551cb9ba7cdc19cea7374775c38
Wyatt92 <vi_hoang@hotmail.de>
2017-08-25 01:00:16 +0200

    #1 Initializing

---
+++ src/main/java/listeners/ReadyListener.java
@@ -0,0 +1,16 @@
+package listeners;
+
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class ReadyListener extends ListenerAdapter{
+
+    public void onReady(ReadyEvent event) {
+        for( Guild g: event.getJDA().getGuilds()) {
+            g.getTextChannelsByName("bot_events", true).get(0).sendMessage(
+                    "WyattLogBot is ready."
+            ).queue();
+        }
+    }
+}
---
+++ src/main/java/util/SECRETS.java
@@ -0,0 +1,6 @@
+package util;
+
+public class SECRETS {
+
+    public static final String TOKEN = "MzUwNDA5Mzk3MjMyNDY3OTc5.DIDnvQ.sGBcDpMlMfOIV7pI3GwwaK5d0MM";
+}
